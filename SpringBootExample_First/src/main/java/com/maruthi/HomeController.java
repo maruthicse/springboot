1add0bc8685e1711eede42a9bdce7c0784b79d15
maruthimcse06@gmail.com <maruthimcse06@gmail.com>
2020-06-02 19:02:51 -0400

    test

---
+++ SpringBootExample_First/src/main/java/com/maruthi/HomeController.java
@@ -0,0 +1,11 @@
+package com.maruthi;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class HomeController {  
+    @RequestMapping("/hello")  
+    public String hello(){  
+        return "Hello Spring Boot, how are you?!";  
+    }  
+}  
---
+++ SpringBootExample_First/src/main/java/com/maruthi/SpringBootExample.java
@@ -0,0 +1,11 @@
+package com.maruthi;
+
+import org.springframework.boot.*;  
+import org.springframework.boot.autoconfigure.*;  
+  
+@SpringBootApplication  
+public class SpringBootExample {  
+    public static void main(String[] args) {  
+        SpringApplication.run(SpringBootExample.class, args);  
+    }  
+}  
